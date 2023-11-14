3b43d8dfeab466c5dcc60476dbb2eeb2e6765ee8
jiheunkim <ziheun54@gmail.com>
2023-11-14 10:40:18 +0900

    Feat: ToolsBox Drag 1차 구현 - ToolsBox 전체에 대해 마우스 드레그로 원하는 위치로 이동 가능

--- frontend/src/Components/Tools.tsx
+++ frontend/src/Components/Tools.tsx
@@ -11,47 +11,72 @@ import { FaWandMagicSparkles, FaEllipsis } from "react-icons/fa6";
 const buttonTailwind =
   "m-1 p-2 hover:bg-gray-700 rounded-lg transition-all text-white text-lg";
 
-// Todo : 타입 수정해야 함, drag 구현
+// 전체 ToolsBox가 다 drag 가능한 형태여서 수정 필요
 function Tools({ mousePosition }: any) {
   const toolDivRef = useRef<HTMLDivElement>(null);
+  const [{ x, y }, setPosition] = useState({
+    x: 0,
+    y: 0,
+  });
   const [toolPosition, setToolPosition] = useState({
     right: 20,
-    top: "calc((100% - 150px)/2)",
+    top: "calc((100% - 1100px)/2)",
   });
 
   return (
-    <div
-      className="pb-2 flex flex-col absolute bg-gray-900 dark:bg-gray-800 transition-all"
-      style={{ ...toolPosition, borderRadius: "12px" }}
+    <div style={{ transform: `translateX(${x}px) translateY(${y}px)` }}
+      onMouseDown={(clickEvent: React.MouseEvent<Element, MouseEvent>) => {
+        const mouseMoveHandler = (moveEvent: MouseEvent) => {
+          const deltaX = moveEvent.screenX - clickEvent.screenX;
+          const deltaY = moveEvent.screenY - clickEvent.screenY;
+
+          setPosition({
+            x: x + deltaX,
+            y: y + deltaY,
+          });
+        };
+
+        const mouseUpHandler = () => {
+          document.removeEventListener('mousemove', mouseMoveHandler);
+        };
+
+        document.addEventListener('mousemove', mouseMoveHandler);
+        document.addEventListener('mouseup', mouseUpHandler, { once: true });
+      }}
     >
       <div
-        // onMouseDown={(e) => {
-        //   setToolPosition({
-        //     left: mousePosition.x,
-        //     top: mousePosition.y,
-        //   });
-        // }}
-        // onMouseUp={(e) => {
-        //   setToolPosition({
-        //     left: mousePosition.x,
-        //     top: mousePosition.y,
-        //   });
-        // }}
-        className="cursor-pointer py-1 flex justify-center items-center text-white"
+        className="pb-2 flex flex-col absolute bg-gray-900 dark:bg-gray-800 transition-all"
+        style={{ ...toolPosition, borderRadius: "12px" }}
       >
-        <FaEllipsis />
+        <div   
+          // onMouseDown={(e) => {
+          //   setToolPosition({
+          //     left: mousePosition.x,
+          //     top: mousePosition.y,
+          //   });
+          // }}
+          // onMouseUp={(e) => {
+          //   setToolPosition({
+          //     left: mousePosition.x,
+          //     top: mousePosition.y,
+          //   });
+          // }}
+          className="cursor-pointer py-1 flex justify-center items-center text-white"
+        >
+          <FaEllipsis />
+        </div>
+        <button className={buttonTailwind} onClick={() => console.log("FaMousePointer 클릭됨")}>
+          <FaMousePointer />
+        </button>
+
+        <button className={buttonTailwind} onClick={() => console.log("FaHandPaper 클릭됨")}>
+          <FaHandPaper />
+        </button>
+
+        <button className={buttonTailwind} onClick={() => console.log("FaWandMagicSparkles 클릭됨")}>
+          <FaWandMagicSparkles />
+        </button>
       </div>
-      <button className={buttonTailwind}>
-        <FaMousePointer />
-      </button>
-
-      <button className={buttonTailwind}>
-        <FaHandPaper />
-      </button>
-
-      <button className={buttonTailwind}>
-        <FaWandMagicSparkles />
-      </button>
     </div>
   );
 }
