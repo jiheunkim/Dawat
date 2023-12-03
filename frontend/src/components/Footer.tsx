a30f5d4dde0cd9e4e02e05ef1743808f683b22ae
jiheunkim <ziheun54@gmail.com>
2023-12-03 19:35:45 +0900

    Fix: 이미지 파일명 변경

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
           <img
             className="mr-1 h-6 sm:h-9"
             alt="logo"
-            src={process.env.PUBLIC_URL + '/img/dawat_logo_p.png'}
+            src="/img/dawat_logo_2.png"
           />
           <p className="text-center text-2xl mb-2">DAWAT</p>
         </div>
